6fb093f9646fa81338fd408537374a9d85ffde2a
masa.bursac <bursac.ra133.2018@uns.ac.rs>
2025-03-06 11:33:52 +0100

    refactoring for entering multiple students; refactoring of output
    nested everything in loop

--- StudentDatabaseApp/src/app/StudentDatabaseApp.java
+++ StudentDatabaseApp/src/app/StudentDatabaseApp.java
@@ -1,12 +1,26 @@
 package app;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		Student student = new Student();
-		student.enroll();
-		student.payTuition();
-		System.out.println(student.showInfo());
+		
+		System.out.print("Enter number of new students to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
+		
+		for(int n = 0; n < numOfStudents; n++) {
+			System.out.println("---------------------");
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+		}
+		System.out.println("ADDED STUDENTS:");
+		for(int n = 0; n < numOfStudents; n++) {
+			System.out.println(students[n].showInfo()+"\n---------------------");
+		}
 	}
 
 }

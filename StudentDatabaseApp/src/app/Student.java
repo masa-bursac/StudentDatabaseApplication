f98fc5bb803243afe938b8909bd67a8239af3fba
masa.bursac <bursac.ra133.2018@uns.ac.rs>
2025-03-06 10:38:27 +0100

    basic constructor for student implemented; properties added to class

--- StudentDatabaseApp/src/app/Student.java
+++ StudentDatabaseApp/src/app/Student.java
@@ -1,5 +1,27 @@
 package app;
 
-public class Student {
+import java.util.Scanner;
 
+public class Student {
+	private String name;
+	private String surname;
+	private int year;
+	private int id;
+	private int tuitionBalance;
+	private String courses;
+	private int costOfCourse = 600;
+	
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter student first name:");
+		this.name = in.nextLine();
+		
+		System.out.println("Enter student last name:");
+		this.surname = in.nextLine();
+		
+		System.out.println("Enter student class level:\n1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senor\nEnter:");
+		this.year = in.nextInt();
+		
+		System.out.println(name + " " + surname + " " + year);
+	}
 }
--- StudentDatabaseApp/src/app/StudentDatabaseApp.java
+++ StudentDatabaseApp/src/app/StudentDatabaseApp.java
@@ -3,8 +3,7 @@ package app;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Student student = new Student();
 	}
 
 }
